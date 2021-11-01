28b9efd5de028659ddb02daaeb00ac1e2c9d49ce
Nguyen Minh Duc <beobiebom@Nguyens-MacBook-Pro.local>
2021-11-01 10:38:54 +0700

    complete onboard screen but error in page 2 :boom:

--- flutter_frontend/lib/core/theme/palette.dart
+++ flutter_frontend/lib/core/theme/palette.dart
@@ -4,12 +4,14 @@ class Palette {
   static const Color aliceBlue = Color(0xFFF4F8FD);
   static const Color darkAliceBlue = Color(0xFFECF2FF);
   static const Color zodiacBlue = Color(0xFF0F2D52);
+  static const Color crayolaBlue = Color(0xFF1773EA);
   static const Color celticBlue = Color(0xFF2673D1);
 
   static const Color lighterBlack = Color(0xFF657181);
-  static const Color crayolaBlue = Color(0xFF1773EA);
   static const Color metallicViolet = Color(0xFF610094);
 
   static const Color sweetRed = Color(0xFFFF725E);
   static const Color orangeRed = Color(0xFFFF4A4A);
+
+  static const Color americanSilver = Color(0xFFCDD0D4);
 }
