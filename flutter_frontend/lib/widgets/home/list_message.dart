f887dd3e228c1a2fcd7c8b4a5990ff239331dd97
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-11-05 16:28:23 +0700

    handle ontap message event :zap: :zap: :zap:

--- flutter_frontend/lib/widgets/home/list_message.dart
+++ flutter_frontend/lib/widgets/home/list_message.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/controller/home/home_controller.dart';
 import 'package:flutter_frontend/core/constants/font_family.dart';
 import 'package:flutter_frontend/core/theme/palette.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class ListMessage extends StatelessWidget {
+  final HomeController homeController = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
@@ -36,61 +39,64 @@ class ListMessage extends StatelessWidget {
                   padding: const EdgeInsets.only(
                     bottom: 6.0,
                   ),
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Palette.americanSilver,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: SizedBox(
-                      height: ScreenUtil().setHeight(65),
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          left: 10,
-                          top: 5,
-                          bottom: 5,
-                        ),
-                        child: Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 40,
-                              backgroundImage: NetworkImage('https://yt3.ggpht.com/ytc/AKedOLQ6aeVpATJCBdgFA8uBSwIuvI0sVGiZ7LSwnqBT=s900-c-k-c0x00ffffff-no-rj'),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Flexible(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Quốc Đạt",
-                                    style: TextStyle(
-                                      color: Palette.zodiacBlue,
-                                      fontWeight: FontWeight.w700,
-                                      fontFamily: FontFamily.fontNunito,
-                                      fontSize: ScreenUtil().setSp(18),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 2,
-                                  ),
-                                  Text(
-                                    "Mi push code lên chưa?",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w700,
-                                      fontFamily: FontFamily.fontNunito,
-                                      fontSize: ScreenUtil().setSp(15),
-                                    ),
-                                  )
-                                ],
+                  child: GestureDetector(
+                    onTap: homeController.onTapMessage,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Palette.americanSilver,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: SizedBox(
+                        height: ScreenUtil().setHeight(65),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: 10,
+                            top: 5,
+                            bottom: 5,
+                          ),
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 40,
+                                backgroundImage: NetworkImage('https://yt3.ggpht.com/ytc/AKedOLQ6aeVpATJCBdgFA8uBSwIuvI0sVGiZ7LSwnqBT=s900-c-k-c0x00ffffff-no-rj'),
                               ),
-                            )
-                          ],
-                        )
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Flexible(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Quốc Đạt",
+                                      style: TextStyle(
+                                        color: Palette.zodiacBlue,
+                                        fontWeight: FontWeight.w700,
+                                        fontFamily: FontFamily.fontNunito,
+                                        fontSize: ScreenUtil().setSp(18),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 2,
+                                    ),
+                                    Text(
+                                      "Mi push code lên chưa?",
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w700,
+                                        fontFamily: FontFamily.fontNunito,
+                                        fontSize: ScreenUtil().setSp(15),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ),
                       ),
                     ),
                   ),
