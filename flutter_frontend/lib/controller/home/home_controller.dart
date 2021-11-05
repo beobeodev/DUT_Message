b2471d7475c6cebfc576e265fd9c565c9e4f6ac3
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-11-05 16:25:30 +0700

    create home controller :sparkles: :sparkles: :sparkles:

--- flutter_frontend/lib/controller/home/home_controller.dart
+++ flutter_frontend/lib/controller/home/home_controller.dart
@@ -1,5 +1,11 @@
+import 'package:flutter_frontend/core/router/router.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
 
+  //Handle event on tap message
+  void onTapMessage() {
+    //Navigate to chat screen with detail message
+    Get.toNamed<dynamic>(GetRouter.chat);
+  }
 }
