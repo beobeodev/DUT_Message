d1c1dc2cf1e22d42f76496b9a1c84fcb7867a74c
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-11-05 14:43:20 +0700

    Fix onboar

--- flutter_frontend/lib/core/router/router.dart
+++ flutter_frontend/lib/core/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_frontend/screens/login.dart';
+import 'package:flutter_frontend/screens/onboard.dart';
 import 'package:flutter_frontend/screens/sign_up.dart';
 import 'package:flutter_frontend/screens/splash.dart';
 import 'package:get/get.dart';
@@ -7,10 +8,12 @@ class GetRouter {
   static const splash = "/splash";
   static const login = "/login";
   static const signUp = "/signUp";
+  static const onboard = "/onboard";
 
   static List<GetPage> pages = [
     GetPage<dynamic>(name: login, page: () => LoginScreen()),
     GetPage<dynamic>(name: signUp, page: () => SignUpScreen()),
     GetPage<dynamic>(name: splash, page: () => SplashScreen()),
+    GetPage<dynamic>(name: onboard, page: () => OnboardScreen()),
   ];
 }
