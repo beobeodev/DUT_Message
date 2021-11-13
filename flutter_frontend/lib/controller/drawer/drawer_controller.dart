f0a773ce376fff001b02483a1a9a28730d5fe847
beobiebom <beobiebom@gmail.com>
2021-11-13 09:12:37 +0700

    create menu item in drawer and refactor :hammer: :hammer:

--- flutter_frontend/lib/controller/drawer/drawer_controller.dart
+++ flutter_frontend/lib/controller/drawer/drawer_controller.dart
@@ -1,11 +1,35 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
 class DrawerScreenController extends GetxController {
+  //Check whether drawer is open or not
   final RxBool isDrawerOpen = false.obs;
+
   final RxDouble xOffset = 0.0.obs;
   final RxDouble yOffset = 0.0.obs;
   final RxDouble scaleFactor  = 1.0.obs;
 
+  //This list to store title and icon of menu item
+  final List<Map<String, dynamic>> listMenuItem = [
+    <String, dynamic>{
+      "title": "Nhắn tin",
+      "icon": FontAwesomeIcons.commentAlt,
+    },
+    <String, dynamic>{
+      "title": "Bạn bè",
+      "icon": FontAwesomeIcons.userFriends
+    },
+    <String, dynamic>{
+      "title": "Hồ sơ",
+      "icon": FontAwesomeIcons.idBadge,
+    },
+    <String, dynamic>{
+      "title": "Kết bạn",
+      "icon": FontAwesomeIcons.userPlus,
+    }
+  ];
+
+  //This function to implement close drawer
   void closeDrawer() {
     xOffset.value = 0.0;
     yOffset.value = 0.0;
@@ -13,6 +37,7 @@ class DrawerScreenController extends GetxController {
     isDrawerOpen.value = false;
   }
 
+  //This function to implement open drawer
   void openDrawer() {
     xOffset.value = 250;
     yOffset.value = 150;
---
+++ flutter_frontend/lib/widgets/drawer/menu_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_frontend/core/constants/font_family.dart';
+import 'package:flutter_frontend/core/theme/palette.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return   DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+        // border: Border.all(
+        //   color: Color(0xFFFF2442),
+        // ),
+      ),
+      child: SizedBox(
+        height: 55,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontFamily: FontFamily.fontNunito,
+                color: Palette.sweetRed,
+                fontSize: ScreenUtil().setSp(18),
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Icon(
+              icon,
+              color: Palette.sweetRed,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
